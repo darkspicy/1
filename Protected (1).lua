99b42e0550028ed15dad9d6ba7438f1976837414
hasdhashdasdas <101713683+darkspicy@users.noreply.github.com>
2025-01-15 00:52:21 +0800

    Update Protected (1).lua

--- Protected (1).lua
+++ Protected (1).lua
@@ -6,11 +6,12 @@ while true do
     }
 
     game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(unpack(args))
+    wait()
     workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
+    wait()
     game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(unpack(args))
-    game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(unpack(args))
+    wait()
     workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(unpack(args))
+    wait()
     game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(unpack(args))
 end
