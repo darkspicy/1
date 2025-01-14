cb5541573b53bad7e1112223e03e78c034a93550
hasdhashdasdas <101713683+darkspicy@users.noreply.github.com>
2025-01-15 04:27:08 +0800

    Update Protected (1).lua

--- Protected (1).lua
+++ Protected (1).lua
@@ -1,25 +1,9 @@
 while true do 
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
     wait()
     local args = {
     [1] = game:GetService("Players").LocalPlayer.Backpack.Nessie
     }
 
     game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(unpack(args))
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
-    workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
-    wait()
     workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
 end
